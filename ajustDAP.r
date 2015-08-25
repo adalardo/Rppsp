61bc6dbc5ce7d3cea617f5a41b5eca25bb7421a7
adalardo <aleadalardo@gmail.com>
2015-08-25 18:08:30 -0300

    possivel escolher um ou varios censos na funcao adjDAP

--- ajustDAP.r
+++ ajustDAP.r
@@ -1,37 +1,50 @@
 ###########################################################################
 ## Funcao para calcular area basal e o dap corrigido para fustes multiplos
-## Alexandre Adalardo de Oliveira, versao: 09 novembro 2009
-#log: calcula area basal para individuos em parcelas distintas onde pode haver repeticao do numero "tag' (abasal.parcela)
-# resolvido problema de somatoria da area basal de fustes multiplos
-#options(help_type="html")
+## Alexandre Adalardo de Oliveira, versao: 25 agosto 2015
 ##########################
-#### DAP da base de dados
+#### DAP na base de dados
 ##########################
-## fustes distintos de mesmo individuo separados por ;
-######################################################
-# duas opcoes 
-### formato longo: cada fuste uma linha
+## fustes distintos de mesmo individuo separados por ";"
+#########################################################
+# modificando para formato longo
+###  cada fuste uma linha
 ### DADOS TESTE  #
-                                        #
 #dados<- data.frame(sp= rep(c("sp1", "sp2", "sp3"), 4), dbh01.mm= rep(c("110;12;130;14", "140;13;12", "14;13", "14"), 3), dbh02.mm= rep(c("150;16;17;18", "NA", "18", "18"), 3) , alt=seq(10, 50, len=12), date01="10-10-2002", date02="10-10-2005", status01= "A", status02 =sample(c(rep("A", 10), "D","D")), stringsAsFactors = FALSE, tag=paste(0, 1:12, sep="") )
-?rep
 adjDAP<- function(dados, dap= "dbh", censos= c("01","02"), sufixo= ".mm")
     {
         vrep <- Vectorize(rep.int, "times")
         namedbh=paste(dap, censos,sufixo, sep="")
+        namedados <- names(dados)
+        dbhano <- names(dados)[grep("dbh", names(dados))]
+        nc <-substr(dbhano[! dbhano %in% namedbh], start=4, stop=5)
+        for (k in nc)
+            {
+                npos <- grep(k, names(dados))
+                dados <- dados[, - npos]
+            }
         dbh =dados[,namedbh]
+        dbh[dbh=="" | dbh==" " | is.null(dbh)]<-NA
         ncensos = length(censos)
         ntree = dim(dados)[1]
-        ldbh0 <- apply(dbh, 2,strsplit, split=";")                 
         nfuste <- matrix(NA, ncol=ncensos,nrow=ntree)
-        for(i in 1:ncensos)
-             {
-                 nfuste[,i]<- sapply(ldbh0[[i]], length)
-             }
-        maxfuste <- apply(nfuste, 1, max)
-        diffuste <- maxfuste - nfuste
-        difvf <- diffuste>0
-        tdif<-apply(difvf>0, 1, sum)
+        if(ncensos>1)
+            {
+                ldbh0 <- apply(dbh, 2,strsplit, split=";")
+                for(i in 1:ncensos)
+                    {
+                        nfuste[,i]<- sapply(ldbh0[[i]], length)
+                    }
+                maxfuste <- apply(nfuste, 1, max)
+                diffuste <- maxfuste - nfuste
+                difvf <- diffuste>0
+                tdif<-apply(difvf>0, 1, sum)
+            }else
+                {
+                    ldbh0 <- strsplit(dbh, split=";")
+                    nfuste[,1]<-sapply(ldbh0, length)
+                    maxfuste <- apply(nfuste, 1, max)
+                    tdif<-rep(0, ntree)
+                }
         datanew=NULL
         for(j in 1:ntree)
             {
@@ -49,14 +62,20 @@ adjDAP<- function(dados, dap= "dbh", censos= c("01","02"), sufixo= ".mm")
                                 fusteNA <- sapply(fusteNA, paste, collapse="")
                                 dbh[j,] <- paste(dbh[j,], fusteNA, sep="")
                             }
-                       
                         ndtree <- paste(rep("dtree", nf), collapse=", " )
                         strtree<-paste("dtree <- rbind(",ndtree,")", sep="")
                         eval(parse(text=strtree))
                         dtree$fuste = 1:nrow(dtree)
                      }
                 dtree[,namedbh]<-NA
-                newdbh <-unlist(sapply(dbh[j,],strsplit, split=";"))
+                if(ncensos>1)
+                    {
+                    newdbh <-unlist(sapply(dbh[j,],strsplit, split=";"))    
+                    }
+                else
+                    {
+                    newdbh <-unlist(sapply(dbh[j],strsplit, split=";"))     
+                    }
                 newdbh[newdbh=="NA"]<-NA
                 dtree[,namedbh]<-as.numeric(newdbh)
                 datanew=rbind(datanew,dtree)
@@ -65,9 +84,12 @@ adjDAP<- function(dados, dap= "dbh", censos= c("01","02"), sufixo= ".mm")
         return(datanew)
     }
 ## debug(adjDAP)
-## adjDAP(dados)
+## adjDAP(dados, censos=c("01", "02"))
 #dados.fuste<- adjDAP(dados)
 #################################################################################################
+#### transforma dados no formato longo em formato de fuste unico com dap recalculado da soma das
+#### areas basais de todos os fuste
+######################################
 fusteUnico <- function(dados.fuste, tag= "tag", dap= "dbh", censos= c("01","02"), sufixo= ".mm")
 {
     namedbh=paste(dap, censos,sufixo, sep="")
@@ -79,6 +101,3 @@ fusteUnico <- function(dados.fuste, tag= "tag", dap= "dbh", censos= c("01","02")
     dados.cm[, namedbh.new]<- abasal.cm[, -1]
     return(dados.cm)
 }
-
-
-
